1d693e7d9a98ec64ba6e1691a8ec500e729be56f
kangkujun <donato14@naver.com>
2023-03-10 13:45:08 +0900

    getStaticPrps 구현

--- nextjs-blog/components/layout.js
+++ nextjs-blog/components/layout.js
@@ -36,7 +36,7 @@ export default function Layout({ children, home}) {
           <>
             <Image 
               priority
-              src='/images/profile.jpg'
+              src='/public/images/80881138.jpeg'
               className={utilStyles.borderCircle}
               height={144}
               width={144}
